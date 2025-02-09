adbf70f9b07b21f6e205a756c6fbc0f5b4d788b1
sinder <smirnov.sinder@gmail.com>
2025-02-09 18:27:37 +0800

    added docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,18 @@
+"""
+CP1404 - Practical
+Get a password with minimum length and display it as asterisks
+"""
 PASSWORD_LENGTH = 10
 
 
 def main():
+    """Get user password and print password as asterisks"""
     password = get_password()
     print_asterisks(password)
 
 
 def get_password():
+    """Get password with a minimum length requirement"""
     password = input('Enter a password: ')
     while len(password) < PASSWORD_LENGTH:
         print(f'Password must be at least {PASSWORD_LENGTH} characters long.')
@@ -15,6 +21,7 @@ def get_password():
 
 
 def print_asterisks(password):
+    """Print as many asterisks as there are in a password"""
     print("*" * len(password))
 
 
