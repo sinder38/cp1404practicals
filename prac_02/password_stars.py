f9045c39aef0dde125da65a6103206a05da55774
sinder <smirnov.sinder@gmail.com>
2025-02-09 18:18:20 +0800

    Refactored password stars program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,22 @@
 PASSWORD_LENGTH = 10
 
-password = input('Enter a password: ')
-while len(password) < PASSWORD_LENGTH:
-    print(f'Password must be at least {PASSWORD_LENGTH} characters long.')
-    password = input('Enter a password: ')
 
-print("*" * len(password))
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def get_password():
+    password = input('Enter a password: ')
+    while len(password) < PASSWORD_LENGTH:
+        print(f'Password must be at least {PASSWORD_LENGTH} characters long.')
+        password = input('Enter a password: ')
+    return password
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+if __name__ == '__main__':
+    main()
