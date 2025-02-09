49bba35db873277666c07173f8fe05e02acbb767
sinder <smirnov.sinder@gmail.com>
2025-02-09 18:29:25 +0800

    added a warning changed PASSWORD_LENGTH to MIN_PASSWORD_LENGTH

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,7 +2,7 @@
 CP1404 - Practical
 Get a password with minimum length and display it as asterisks
 """
-PASSWORD_LENGTH = 10
+MIN_PASSWORD_LENGTH = 10
 
 
 def main():
@@ -10,16 +10,16 @@ def main():
     password = get_password()
     print_asterisks(password)
 
-
 def get_password():
     """Get password with a minimum length requirement"""
     password = input('Enter a password: ')
-    while len(password) < PASSWORD_LENGTH:
-        print(f'Password must be at least {PASSWORD_LENGTH} characters long.')
+    while len(password) < MIN_PASSWORD_LENGTH:
+        print(f'Password must be at least {MIN_PASSWORD_LENGTH} characters long.')
         password = input('Enter a password: ')
     return password
 
 
+# WARN: should have made a print_symbol function, maybe I don't like asterisks
 def print_asterisks(password):
     """Print as many asterisks as there are in a password"""
     print("*" * len(password))
