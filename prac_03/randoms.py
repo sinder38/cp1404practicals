791907a31c01aa56ce212a5d25183465412c652f
sinder <smirnov.sinder@gmail.com>
2025-02-16 18:21:38 +0800

    Copy questions from the task Answer all questions

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -3,3 +3,30 @@ import random
 print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
+
+# What did you see on line 1?
+# 8
+
+# What was the smallest number you could have seen, what was the largest?
+# Smallest: 6
+# Largest: 14
+
+# What did you see on line 2?
+# 3
+
+# What was the smallest number you could have seen, what was the largest?
+# Smallest: 3
+# Largest: 9
+
+# Could line 2 have produced a 4?
+# No. Only in you nightmares
+
+# What did you see on line 3?
+# 3.0993525104300343
+
+# What was the smallest number you could have seen, what was the largest?
+# Smallest: 2.697028686104593
+# LargesT: 5.490302539821558
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+print(random.randint(1, 100)) # didn't ask for type
