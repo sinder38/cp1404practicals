a3ebf544f4adc43790ee818eaeead045e74b1e1c
sinder <smirnov.sinder@gmail.com>
2025-02-16 19:12:37 +0800

    Add cool version for no reason

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -11,3 +11,14 @@ while not is_finished:
     except ValueError:
         print("Please enter a valid integer.")
 print("Valid result is:", result)
+
+
+# NOTE: I like this one much more
+def get_valid_number(_type=int):
+    """get valid number from user of appropriate type"""
+    while True: # yes
+        user_input = input(f"Enter a valid {_type.__name__}: ")
+        try:
+            return _type(user_input)
+        except ValueError:
+            print(f"Invalid input. Please enter a valid {_type.__name__}.")
