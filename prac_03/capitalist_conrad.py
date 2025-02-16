b48ab10d9218c2f45c93280125b436055573ed8d
sinder <smirnov.sinder@gmail.com>
2025-02-16 18:46:06 +0800

    Rewrite capitalist conrad to use main function Change open close to with open

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -18,25 +18,31 @@ INITIAL_PRICE = 10.0
 
 FILENAME = "capitalist_conrad.txt"
 
-price = INITIAL_PRICE
-day = 0
 
-out_file = open(FILENAME, 'w')
-print(f"Starting price ${price:,.2f}", file=out_file)
-while MIN_PRICE <= price <= MAX_PRICE:
-    price_change = 0
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
-    if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
-        price_change = random.uniform(0, MAX_INCREASE)
-    else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
-        price_change = random.uniform(-MAX_DECREASE, 0)
+def main():
+    price = INITIAL_PRICE
+    day = 0
 
-    price *= (1 + price_change)
-    day += 1
-    print(f"On day {day} price is: ${price:,.2f}", file=out_file)
-out_file.close()
+    # with open is a superior way to do this
+    with open(FILENAME, 'w') as out_file:
+        print(f"Starting price ${price:,.2f}", file=out_file)
+        while MIN_PRICE <= price <= MAX_PRICE:
+            price_change = 0
+            # generate a random integer of 1 or 2
+            # if it's 1, the price increases, otherwise it decreases
+            if random.randint(1, 2) == 1:
+                # generate a random floating-point number
+                # between 0 and MAX_INCREASE
+                price_change = random.uniform(0, MAX_INCREASE)
+            else:
+                # generate a random floating-point number
+                # between negative MAX_DECREASE and 0
+                price_change = random.uniform(-MAX_DECREASE, 0)
+
+            price *= (1 + price_change)
+            day += 1
+            print(f"On day {day} price is: ${price:,.2f}", file=out_file)
+
+
+if __name__ == "__main__":
+    main()
