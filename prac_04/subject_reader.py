65cf96ead716d56a3d05d1cb1218371ba862079e
sinder <smirnov.sinder@gmail.com>
2025-02-23 19:17:19 +0800

    Remove debug prints Improve formatting

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -16,11 +16,8 @@ def load_data():
     subjects = []
     input_file = open(FILENAME)
     for line in input_file:
-        # print(line)  # See what a line looks like
-        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         subjects.append(parts)  # NOTE: yield opportunity
     input_file.close()
@@ -32,5 +29,5 @@ def print_subjects(subjects):
     for subject in subjects:
         print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
 
-
-main()
+if __name__ == '__main__':
+    main()
