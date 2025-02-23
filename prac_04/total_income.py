5c41331d00c000a7de1ff1e0738506805c4c3f35
sinder <smirnov.sinder@gmail.com>
2025-02-23 19:06:33 +0800

    Move report in its own function Change to enumerate instead of a counter Remove redundant str Add more formatting

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -10,15 +10,19 @@ def main():
     month_count = int(input("How many months? "))
 
     for month in range(1, month_count + 1):
-        income = float(input(f"Enter income for month {str(month)}: "))
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    print_report(incomes)
+
+def print_report(incomes):
+    """Print income report"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, month_count + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes, 1):
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
 
 
-main()
+if __name__ == "__main__":
+    main()
