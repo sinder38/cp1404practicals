1a6bc48b376060c70a577e8054b1ac2e376d513c
sinder <smirnov.sinder@gmail.com>
2025-02-23 19:23:49 +0800

    Complete list exercise

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,3 +2,28 @@
 CP1404/CP5632 Practical
 List Exercises
 """
+
+
+def main():
+    numbers = get_numbers()
+    print_numbers_data(numbers)
+
+
+def get_numbers(count=5):
+    numbers = []
+    for i in range(count):
+        number = int(input("Number: "))
+        numbers.append(number)
+    return numbers
+
+
+def print_numbers_data(numbers):
+    print("The first number is", numbers[0])
+    print("The last number is", numbers[-1])
+    print("The smallest number is", min(numbers))
+    print("The largest number is", max(numbers))
+    print("The average of the numbers is", sum(numbers) / len(numbers))
+
+
+if __name__ == '__main__':
+    main()
