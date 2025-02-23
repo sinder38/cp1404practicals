51d8c56e224e82f59e9d420138ddbf691626b51c
sinder <smirnov.sinder@gmail.com>
2025-02-23 20:01:26 +0800

    Write the whole program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -2,3 +2,43 @@
 CP1404/CP5632 Practical
 Quick picks
 """
+
+from random import randint
+
+NUMBERS_PER_LINE = 6
+
+# Random generation:
+MIN_RANGE = 1
+MAX_RANGE = 45
+
+
+def main():
+    """Quick picks program - choose sets of random numbers."""
+    quick_pick_count = get_valid_quick_picks()
+
+    for i in range(quick_pick_count):
+        sorted_quick_pick = sorted(generate_quick_pick())
+        print(" ".join(f"{number:2}" for number in sorted_quick_pick))
+
+
+def get_valid_quick_picks():
+    """get valid number from user of quick picks"""
+    quick_picks_count = int(input("How many quick picks? "))
+    while quick_picks_count < 0:
+        print("Error. Cannot be negative")
+        quick_picks_count = int(input("How many quick picks? "))
+    return quick_picks_count
+
+
+def generate_quick_pick():
+    """Generate random number from user of quick pick"""
+    quick_pick = []
+    for j in range(NUMBERS_PER_LINE):
+        number = randint(MIN_RANGE, MAX_RANGE)
+        while number in quick_pick:
+            number = randint(MIN_RANGE, MAX_RANGE)
+        quick_pick.append(number)
+    return quick_pick
+
+
+main()
