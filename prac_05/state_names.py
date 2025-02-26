e3f457adc13822c322f0a84f266c745a04638812
sinder <smirnov.sinder@gmail.com>
2025-02-26 18:43:05 +0800

    Change program to use try except for error checking

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -15,9 +15,9 @@ def main():
 
     state_code = input("Enter short state: ").upper()
     while state_code != "":
-        if state_code in CODE_TO_NAME:
+        try:
             print_code_name_pair(state_code, CODE_TO_NAME[state_code])
-        else:
+        except KeyError:
             print("Invalid short state")
         state_code = input("Enter short state: ").upper()
 
