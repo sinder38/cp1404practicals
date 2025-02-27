b5c46cf7a38103e7d413535092ac1281911f0b82
sinder <smirnov.sinder@gmail.com>
2025-02-27 22:53:13 +0800

    Remove spaces from colour names Improve print message

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,14 +4,14 @@ Hex Colours program
 """
 
 COLOUR_NAME_TO_HEX = {
-    "absolute zero": "#0048ba",
-    "acid green": "#b0bf1a",
-    "alice blue": "#f0f8ff",
-    "alizarin crimson": "#e32636",
+    "absolutezero": "#0048ba",
+    "acidgreen": "#b0bf1a",
+    "aliceblue": "#f0f8ff",
+    "alizarincrimson": "#e32636",
     "amaranth": "#e52b50",
     "amber": "#ffbf00",
     "amethyst": "#9966cc",
-    "antique white": "#faebd7",
+    "antiquewhite": "#faebd7",
     "apricot": "#fbceb1",
     "aqua": "#00ffff"
 }
@@ -21,7 +21,7 @@ def main():
     """Program to match colour name with its hex code"""
     name = input("Enter hex colour name: ").strip().lower()
     try:
-        print(COLOUR_NAME_TO_HEX[name])
+        print(f"Code for {name} is {COLOUR_NAME_TO_HEX[name]}")
     except KeyError:
         print("Unknown colour name")
 
