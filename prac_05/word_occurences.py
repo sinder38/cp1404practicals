0169d4a3410597f7aba27c6e5c36aceb1a6c04fa
sinder <smirnov.sinder@gmail.com>
2025-02-27 23:09:43 +0800

    Improve formatting Fix typo Add docstring

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -6,16 +6,16 @@ Actual:   8.3 minutes
 
 
 def main():
-    string = input("Text: ")
-    lst_str = string.split(" ")
+    """Program that counts occurrences of words in text stirng"""
+    lst_str = input("Text: ").split(" ")
     result = {}
     biggest_len = 0
     for word in lst_str:
         result[word] = result.get(word, 0) + 1
         biggest_len = max(biggest_len, len(word))
 
-    for (word, occurences) in sorted(result.items(), key=lambda x: x[0]):
-        print(f"{word:{biggest_len}} : {occurences}")
+    for (word, occurrences) in sorted(result.items(), key=lambda x: x[0]):
+        print(f"{word:{biggest_len}} : {occurrences}")
 
 
 if __name__ == '__main__':
