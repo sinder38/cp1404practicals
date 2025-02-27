0b55a34108ca64e34c8dc0b7a8753cbdcc3ce167
sinder <smirnov.sinder@gmail.com>
2025-02-27 23:45:11 +0800

    Extract print as a separate function Extract confirm inputs as a constant Remove redundant title list iteration Add helpful comments

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,21 +4,34 @@ Estimate: 10 minutes
 Actual:   11 minutes
 """
 
+CONFIRM_INPUTS = ("y", "")
+
 
 def main():
     """Program to display emails"""
     email_to_name = {}
-    email = input("Enter your email: ")
+    email = input("Email: ")
     while email != "":
+
+        # Extract user's name from the email
         names = email.split("@").pop(0).split(".")
-        formatted_name = " ".join([n.title() for n in names])
-        confirm = input(f"Is your name {formatted_name}? (Y/n) : ")
-        if confirm.lower() not in ["y", ""]:
+        formatted_name = " ".join(names).title()
+
+        # Confirm automatically extracted name
+        confirm = input(f"Is your name {formatted_name}? (Y/n) ")
+        if confirm.lower() not in CONFIRM_INPUTS:
             formatted_name = input("Name: ")
+
         email_to_name[email] = formatted_name
+
         email = input("Enter your email: ")
 
     print()
+    print_email_data(email_to_name)
+
+
+def print_email_data(email_to_name):
+    """Print all email to name in a pretty way """
     for email, formatted_name in email_to_name.items():
         print(f"{formatted_name} ({email})")
 
