72568a099f1fa5d74a03d6d742b57b7f00096bf9
sinder <smirnov.sinder@gmail.com>
2025-02-27 23:07:15 +0800

    Add solution to word occurences program

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,5 +1,22 @@
 """
 Word Occurrences
 Estimate: 10  minutes
-Actual:   _ minutes
+Actual:   8.3 minutes
 """
+
+
+def main():
+    string = input("Text: ")
+    lst_str = string.split(" ")
+    result = {}
+    biggest_len = 0
+    for word in lst_str:
+        result[word] = result.get(word, 0) + 1
+        biggest_len = max(biggest_len, len(word))
+
+    for (word, occurences) in sorted(result.items(), key=lambda x: x[0]):
+        print(f"{word:{biggest_len}} : {occurences}")
+
+
+if __name__ == '__main__':
+    main()
