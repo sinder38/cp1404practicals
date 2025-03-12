64d6afc0d8e14d238754b141b1d88bb1122a8d08
sinder <smirnov.sinder@gmail.com>
2025-03-12 12:47:26 +0800

    Complete Guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,6 +3,10 @@ CP1404/CP5632 Practical
 Guitar
 """
 
+from datetime import datetime
+
+VINTAGE_AGE = 50
+
 
 class Guitar:
     """Stores information about a guitar"""
@@ -12,3 +16,19 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of a Guitar class"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+
+    def get_age(self):
+        """Get the age of a guitar based on the current year"""
+        #NOTE: Using a constant for a year as done in the solution is a bad idea
+        # that will make program wrong only after a year. So:
+        current_year = datetime.now().year
+        return current_year - self.year
+
+    def is_vintage(self):
+        """Determine if a Guitar is vintage"""
+        return self.get_age() >= VINTAGE_AGE
