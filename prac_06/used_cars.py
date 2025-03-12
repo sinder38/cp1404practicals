361e2e0cdd98eba6a13d33bc69dbbf90c23e02da
sinder <smirnov.sinder@gmail.com>
2025-03-12 11:59:47 +0800

    Complete modifications from 1-4

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,10 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
-    print(my_car)
+    limo = Car(100)
+    limo.add_fuel(20)
+    print(f"Car has fuel: {limo.fuel}")
+    limo.drive(115)
 
-
-main()
+if __name__ == "__main__":
+    main()
