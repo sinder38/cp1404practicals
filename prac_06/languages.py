32df25aa80a0f7d043328c27a325f43d42a964c6
sinder <smirnov.sinder@gmail.com>
2025-03-12 12:28:06 +0800

    Prepare for languages task Add estimated time

---
+++ prac_06/languages.py
@@ -0,0 +1,11 @@
+"""
+Languages
+Estimate: 10 minutes
+Actual:   _ minutes
+"""
+
+
+# python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+# ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+# visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+# print(python)
