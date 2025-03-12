7c667c52ef24e3401f704d7959d254f7a29d6aef
sinder <smirnov.sinder@gmail.com>
2025-03-12 12:36:29 +0800

    Complete Languages task Implement ProgrammingLanguage class Add a few small test of its functionality following the task

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,11 +1,17 @@
 """
 Languages
 Estimate: 10 minutes
-Actual:   _ minutes
+Actual:   7 minutes
 """
+from prac_06.programming_language import ProgrammingLanguage
 
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+languages = (python, ruby, visual_basic)
+print(python)
 
-# python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-# ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-# visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-# print(python)
+print("The dynamically typed languages are:")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
