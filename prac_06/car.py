96cc8b395d7acecb25a5902406e60dff24850c64
sinder <smirnov.sinder@gmail.com>
2025-03-12 12:23:26 +0800

    Add name to Car class Complete All modifications

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,17 +4,19 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+         name: string, reference name for car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
     def __str__(self):
         """Return a string representation of a Car object"""
-        return f"Car, fuel={self.fuel}, odometer={self._odometer}"
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,12 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    limo = Car(100)
+    limo = Car("Limo", 100)
     limo.add_fuel(20)
     print(f"Car has fuel: {limo.fuel}")
     limo.drive(115)
+    print(limo)
+
 
 if __name__ == "__main__":
     main()
