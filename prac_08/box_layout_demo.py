12f7e93cb9cbebcd82e57702797f1a4f0175375b
sinder <smirnov.sinder@gmail.com>
2025-03-29 13:45:31 +0800

    Add docstrings to the demo Comment out debug print

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,16 +3,21 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Demo app showcasing BoxLayout functionality"""
+
     def build(self):
+        """Build and return the root widget from KV file"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
-        print('test')
+        """Update label with greeting using input text"""
+        # print('test')
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clear both input and output fields"""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
