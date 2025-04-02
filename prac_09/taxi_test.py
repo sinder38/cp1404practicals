ed8b89a3c07f161a12b2cb758d4813462f00fd88
sinder <smirnov.sinder@gmail.com>
2025-04-02 11:51:52 +0800

    Move price_per_km parameter to a class variable

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -8,7 +8,7 @@ from prac_09.taxi import Taxi
 
 def main():
     """Test Taxi class."""
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
