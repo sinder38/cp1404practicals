d0a5d5d3c16fb71b7de90107462fe527a6609b92
sinder <smirnov.sinder@gmail.com>
2025-04-02 13:36:41 +0800

    Add note about get_number Implement "d" choice option

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -12,6 +12,9 @@ CHOICE_PROMPT = ">>> "
 
 
 def main():
+    current_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    total_cost = 0.0
     print("Let's drive!")
     print(MENU)
     menu_choice = input(CHOICE_PROMPT).lower()
@@ -19,13 +22,23 @@ def main():
         if menu_choice == "c":
             pass
         elif menu_choice == "d":
-            pass
+            if current_taxi:
+                current_taxi.start_fare()
+                distance_to_drive = get_number("Drive how far? ", min_value=0, input_type=float)
+                current_taxi.drive(distance_to_drive)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+                total_cost += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid option")
         print(MENU)
         menu_choice = input(CHOICE_PROMPT).lower()
 
 
+# NOTE: This function is from previous practical.
+# I don't want to import from previous practicals because I want each practical to be independent of another
 def get_number(prompt="", min_value=None, max_value=None, input_type: type = int, skip_capability=False):
     """Gets valid number from the user, inclusive minimum and maximum accepted can be adjusted.
      Skip capability allows user to skip the prompt by pressing enter, the function will return None"""
